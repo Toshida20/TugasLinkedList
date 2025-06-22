a581e7f75ef3547c7e448ca0cf68c6b83c9e622f
Toshida20 <Ilhamm1979@gmail.com>
2025-06-22 14:24:05 +0800

    Penambahan struct awal toko

---
+++ InventoryKomputer.cpp/InventoryTokoKomputer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//InventoryTokoKomputer
+
+struct toko{
+    int id;
+    string nama;
+    string kategori;
+    string harga;
+    int stok;
+    toko* next;
+};
+
+int main(){
+
+}
