c32f258524bc647e475c55ecb2cb4845cc60c68b
Toshida20 <Ilhamm1979@gmail.com>
2025-06-22 14:36:16 +0800

    Membuat node untuk toko komputer

--- InventoryKomputer.cpp/InventoryTokoKomputer.cpp
+++ InventoryKomputer.cpp/InventoryTokoKomputer.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 //InventoryTokoKomputer
+//C030324062_STRUCT
+//git branch -M main
+//git push -u origin main
+//git add C030324062_STRUCT.cpp
+//git commit -m "BLA BLA BLA"
 
 struct toko{
     int id;
@@ -12,6 +17,17 @@ struct toko{
     int stok;
     toko* next;
 };
+
+toko* listBarang(int id, string nama, string kategori, string harga, int stok) {
+    toko* barangBaru=new toko;
+    barangBaru->id=id;
+    barangBaru->nama=nama;
+    barangBaru->kategori=kategori;
+    barangBaru->harga=harga;
+    barangBaru->stok=stok;
+    barangBaru->next=nullptr;
+    return barangBaru;
+}
 
 int main(){
 
